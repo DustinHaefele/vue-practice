3911b1048788ec6fccbe3b60e046da9c8fe6fca9
Dustin Haefele <Dustin.haefele@gmail.com>
2020-02-28 13:34:43 -0500

    added some components where you can add and remove candidates from a the list.

--- src/components/MyList.vue
+++ src/components/MyList.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="hello">
     <!-- v-bind allows you to use data within a component where you can't use {{}}  -->
-    <h1 v-bind:title="msg" >Hover over me to see this message</h1>
+    <h1 v-bind:title="msg" v-show="showing">Hover over me to see this message</h1>
+    <ul>
+      <li v-bind:key="idx" v-for="(cand, idx) in candidates">
+        {{cand.name}}
+        <button v-on:click="removeCandidate(idx)">remove</button>
+      </li>
+    </ul>
+
+      <input v-model="newCand" />
+      <button v-on:click="addCandidate()" type='submit'>add Candidate</button>
+
   </div>
 </template>
 
@@ -10,8 +20,30 @@ export default {
   name: "MyList",
   data() {
     return {
-      msg: "this is from data"
+      msg: "Uncle Bernard",
+      newCand: "",
+      showing: true,
+      candidates: [
+        { name: "Biden" },
+        { name: "Trump" },
+        { name: "Uncle Bernie" },
+        { name: "Liz Warren" },
+        { name: "Andrew Yang" }
+      ]
     };
+  },
+  methods: {
+    addCandidate: function() {
+      if(this.newCand.length > 0) {
+      this.candidates.push({ name: this.newCand });
+      }
+    },
+
+    removeCandidate: function(idx) {
+      this.candidates = this.candidates.filter(function(cand, index) {
+        return index !== idx;
+      });
+    }
   }
 };
 </script>
